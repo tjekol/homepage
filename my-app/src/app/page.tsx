dac79fc9c05f65347b5affc76f9606268e02c6ba
Thea Kolnes <thea.jenny02@gmail.com>
2023-11-11 00:16:54 +0100

    added shad ui to project

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -3,19 +3,19 @@ import meg from '@/app/images/cv-bilde.jpeg';
 
 export default function Home() {
 	return (
-		<main className="flex min-h-screen flex-col items-center p-24 bg-primary">
+		<main className="flex min-h-screen flex-col items-center p-24 bg-lightbrown">
 			<div className='w-5/6 flex space-x-4 mb-20'>
-				<h1 className='text-dark grow font-bold'>Thea Jenny E. Kolnes</h1>
-				<h2 className='text-dark flex-none'>Hjem</h2>
-				<h2 className='text-dark flex-none'>Om meg</h2>
-				<h2 className='text-dark flex-none'>Prosjekter</h2>
+				<h1 className='text-darkbrown grow font-bold'>Thea Jenny E. Kolnes</h1>
+				<h2 className='text-darkbrown flex-none'>Hjem</h2>
+				<h2 className='text-darkbrown flex-none'>Om meg</h2>
+				<h2 className='text-darkbrown flex-none'>Prosjekter</h2>
 			</div>
 
-			<div className='w-1/2 bg-secondary rounded-[10px] p-10'>
-				<h1 className='text-dark font-bold text-center'>✨Hei på deg✨</h1>
+			<div className='w-2/3 bg-cream rounded-[10px] p-10 space-y-8'>
+				<h1 className='text-darkbrown font-bold text-center'>✨Hei på deg✨</h1>
 				<div className='flex justify-between space-x-10 items-center'>
 					<Image src={meg.src} width={200} height={200} alt={''} style={{borderRadius: '100px'}}/>
-					<p className='text-lg text-dark'>Mitt navn er Thea Jenny Kolnes. Jeg tar bachelor i Datateknologi💻 i Bergen på UIB.</p>
+					<p className='text-lg text-darkbrown'>Mitt navn er Thea Jenny Kolnes. Jeg tar bachelor i Datateknologi💻 i Bergen på UIB.</p>
 				</div>
 			</div>
 
