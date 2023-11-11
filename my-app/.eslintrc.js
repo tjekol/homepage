a630cf8aa2f87c4206b8ef797723d48931349aff
Thea Kolnes <thea.jenny02@gmail.com>
2023-11-11 01:31:21 +0100

    functional button links

--- my-app/.eslintrc.js
+++ my-app/.eslintrc.js
@@ -1,53 +1,54 @@
 module.exports = {
-	'env': {
-		'browser': true,
-		'es2021': true,
-		'node': true
-	},
-	'extends': [
-		'eslint:recommended',
-		'plugin:@typescript-eslint/recommended',
-		'plugin:react/recommended'
-	],
-	'overrides': [
-		{
-			'env': {
-				'node': true
-			},
-			'files': [
-				'.eslintrc.{js,cjs}'
-			],
-			'parserOptions': {
-				'sourceType': 'script'
-			}
-		}
-	],
-	'parser': '@typescript-eslint/parser',
-	'parserOptions': {
-		'ecmaVersion': 'latest',
-		'sourceType': 'module'
-	},
-	'plugins': [
-		'@typescript-eslint',
-		'react'
-	],
-	'rules': {
-		'indent': [
-			'error',
-			'tab'
-		],
-		'linebreak-style': [
-			'error',
-			'unix'
-		],
-		'quotes': [
-			'error',
-			'single'
-		],
-		'semi': [
-			'error',
-			'always'
-		],
-		'react/react-in-jsx-scope': 'off',
-	}
+  'env': {
+    'browser': true,
+    'es2021': true,
+    'node': true
+  },
+  'extends': [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended'
+  ],
+  'overrides': [
+    {
+      'env': {
+        'node': true
+      },
+      'files': [
+        '.eslintrc.{js,cjs}'
+      ],
+      'parserOptions': {
+        'sourceType': 'script'
+      }
+    }
+  ],
+  'parser': '@typescript-eslint/parser',
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module'
+  },
+  'plugins': [
+    '@typescript-eslint',
+    'react'
+  ],
+  'rules': {
+    'indent': [
+      'error',
+      // 'tab'
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'always'
+    ],
+    'react/react-in-jsx-scope': 'off',
+  }
 };
--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -2,43 +2,46 @@ import Image from 'next/image';
 import meg from '@/app/images/cv-bilde.jpeg';
 import { Button } from '@/components/ui/button';
 import { LinkedInLogoIcon, GitHubLogoIcon, EnvelopeClosedIcon, MoonIcon } from '@radix-ui/react-icons';
+import Link from 'next/link';
+import Header from '@/components/header';
+
 
 export default function Home() {
-	return (
-		<main className="flex min-h-screen flex-col items-center p-10 justify-between bg-primary">
-			<div className='w-5/6 flex space-x-2'>
-				<h1 className='text-secondary grow font-bold'>Thea Jenny E. Kolnes</h1>
-				<Button variant='link' className='text-2xl text-secondary flex-none'>Hjem</Button>
-				<Button variant='link' className='text-2xl text-secondary flex-none'>Om meg</Button>
-				<Button variant='link' className='text-2xl text-secondary flex-none'>Prosjekter</Button>
-				<Button size='icon' variant='ghost' className='text-secondary'>
-					<MoonIcon className='scale-150'/>
-				</Button>
-			</div>
+  return (
+    <main className="flex min-h-screen flex-col items-center p-16 justify-between bg-primary">
+      <Header />
 
-			<div className='w-2/3 bg-cream rounded-[10px] p-10 space-y-8'>
-				<h1 className='text-secondary font-bold text-center uppercase'>✨Hei på deg✨</h1>
-				<div className='flex justify-between space-x-10 items-center'>
-					<Image src={meg.src} width={200} height={200} alt={''} style={{borderRadius: '100px'}}/>
-					<p className='text-lg text-secondary'>Mitt navn er Thea Jenny Kolnes. Jeg tar bachelor i Datateknologi💻 i Bergen på UIB.</p>
-				</div>
-				<div className='flex justify-center space-x-8'>
-					<Button variant='secondary' size='lg' className='text-cream text-base'>
-						<LinkedInLogoIcon className='mr-2 scale-150' />
-						LinkedIn
-					</Button>
-					<Button variant='secondary' size='lg' className='text-cream text-base'>
-						<GitHubLogoIcon className='mr-2 scale-150' />
-						Github
-					</Button>
-					<Button variant='secondary' size='lg' className='text-cream text-base'>
-						<EnvelopeClosedIcon className='mr-2 scale-150' />
-						Mail
-					</Button>
-				</div>
-			</div>
+      <div className='w-2/3 bg-cream rounded-[10px] p-10 space-y-8'>
+        <h1 className='text-secondary font-bold text-center uppercase'>✨Hei på deg✨</h1>
+        <div className='flex justify-center space-x-10 items-center p-8'>
+          <Image src={meg.src} width={200} height={200} alt={''} style={{borderRadius: '100px'}}/>
+          <p className='text-lg text-secondary'>
+						Mitt navn er Thea Jenny Kolnes. Jeg tar bachelor i Datateknologi💻 i Bergen på UIB.
+          </p>
+        </div>
+        <div className='flex justify-center space-x-8'>
+          <Button variant='secondary' size='lg' className='text-cream text-base' asChild>
+            <Link href={'https://www.linkedin.com/in/thea-jenny-kolnes-a79821231/'} target='_blank'>
+              <LinkedInLogoIcon className='mr-2 scale-150' />
+							LinkedIn
+            </Link>
+          </Button>
+          <Button variant='secondary' size='lg' className='text-cream text-base' asChild>
+            <Link href={'https://github.com/tjekol'} target='_blank'>
+              <GitHubLogoIcon className='mr-2 scale-150' />
+							Github
+            </Link>
+          </Button>
+          <Button variant='secondary' size='lg' className='text-cream text-base' asChild>
+            <Link href={'mailto:thea.jenny02@gmail.com'}>
+              <EnvelopeClosedIcon className='mr-2 scale-150' />
+							Mail
+            </Link>
+          </Button>
+        </div>
+      </div>
 
-			<div className='text-secondary'>Made by Thea Jenny Kolnes🦋</div>
-		</main>
-	);
+      <div className='text-secondary'>Made by Thea Jenny Kolnes🦋</div>
+    </main>
+  );
 }
