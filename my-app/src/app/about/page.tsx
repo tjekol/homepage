b51acad7dead1cf6160602a747a8b277e99ffd63
Thea Kolnes <thea.jenny02@gmail.com>
2023-11-12 00:41:25 +0100

    interactive nav bar

--- my-app/src/app/about/page.tsx
+++ my-app/src/app/about/page.tsx
@@ -1,9 +1,11 @@
 
 export default function About() {
   return (
-    <div className='w-full space-y-8 rounded-[10px] bg-secondary/80 px-10 py-20'>
+    <div className='w-full flex flex-col items-center space-y-8 rounded-[10px] bg-secondary/80 px-10 py-20'>
       <h1 className='text-center uppercase'>Om meg✍🏽</h1>
-      <p className='text-center'>Jeg har stor interesse for videoredigering📹, koding👩🏽‍💻, fotografi📸 og kunst🎨.</p>
+      <div className='w-1/2'>
+        <p className='text-center'>Jeg har stor interesse for videoredigering📹, koding👩🏽‍💻, fotografi📸 og kunst🎨.</p>
+      </div>
     </div>
   );
 }
--- my-app/src/components/header.tsx
+++ my-app/src/components/header.tsx
@@ -1,33 +1,42 @@
+'use client';
+
 import Link from 'next/link';
 import { Button } from './ui/button';
 import { MoonIcon } from '@radix-ui/react-icons';
+import { useState } from 'react';
 
 function Header() {
+  const [page, setPage] = useState('/');
+  console.log(page);
+
   return (
     <div className='w-5/6 flex space-x-2'>
       <h2 className='grow'>Thea Jenny E. Kolnes</h2>
       <Button
         variant='link'
-        className='flex-none text-2xl text-text'
+        className={'flex-none text-2xl ' + (page === '/' ? 'text-text' : 'text-primary')}
+        onClick={() => setPage('/')}
         asChild
       >
         <Link href={'/'}>Hjem</Link>
       </Button>
       <Button
         variant='link'
-        className='flex-none text-2xl text-text'
+        className={'flex-none text-2xl ' + (page === '/about' ? 'text-text' : 'text-primary')}
+        onClick={() => setPage('/about')}
         asChild
       >
         <Link href={'/about'}>Om meg</Link>
       </Button>
       <Button
         variant='link'
-        className='flex-none text-2xl text-text'
+        className={'flex-none text-2xl ' + (page === '/projects' ? 'text-text' : 'text-primary')}
+        onClick={() => setPage('/projects')}
         asChild
       >
         <Link href={'/projects'}>Prosjekter</Link>
       </Button>
-      <Button size='icon' variant='ghost' className='text-text'>
+      <Button size='icon' variant='ghost' className='text-text-text-text'>
         <MoonIcon className='scale-150' />
       </Button>
     </div>
