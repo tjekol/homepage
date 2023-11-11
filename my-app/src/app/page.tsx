94e0fe42da3406baac97103611df9fc0aab01415
Thea Kolnes <thea.jenny02@gmail.com>
2023-11-12 00:10:30 +0100

    upt layout, added pages

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -7,71 +7,63 @@ import {
   EnvelopeClosedIcon,
 } from '@radix-ui/react-icons';
 import Link from 'next/link';
-import Header from '@/components/header';
 
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between bg-primary p-16'>
-      <Header />
-
-      <div className='w-2/3 space-y-8 rounded-[10px] bg-cream p-10'>
-        <h1 className='text-center font-bold uppercase text-secondary'>
-          ✨Hei på deg✨
-        </h1>
-        <div className='flex items-center justify-center space-x-10 p-8'>
-          <Image
-            src={meg.src}
-            width={200}
-            height={200}
-            alt={''}
-            style={{ borderRadius: '100px' }}
-          />
-          <p className='text-lg text-secondary'>
-            Mitt navn er Thea Jenny Kolnes. Jeg tar bachelor i Datateknologi💻 i
-            Bergen på UIB.
-          </p>
-        </div>
-        <div className='flex justify-center space-x-8'>
-          <Button
-            variant='secondary'
-            size='lg'
-            className='text-base text-cream'
-            asChild
-          >
-            <Link
-              href={'https://www.linkedin.com/in/thea-jenny-kolnes-a79821231/'}
-              target='_blank'
-            >
-              <LinkedInLogoIcon className='mr-2 scale-150' />
-              LinkedIn
-            </Link>
-          </Button>
-          <Button
-            variant='secondary'
-            size='lg'
-            className='text-base text-cream'
-            asChild
-          >
-            <Link href={'https://github.com/tjekol'} target='_blank'>
-              <GitHubLogoIcon className='mr-2 scale-150' />
-              Github
-            </Link>
-          </Button>
-          <Button
-            variant='secondary'
-            size='lg'
-            className='text-base text-cream'
-            asChild
-          >
-            <Link href={'mailto:thea.jenny02@gmail.com'}>
-              <EnvelopeClosedIcon className='mr-2 scale-150' />
-              Mail
-            </Link>
-          </Button>
-        </div>
+    <div className='w-full space-y-8 rounded-[10px] bg-secondary/80 px-10 py-20'>
+      <h1 className='text-center uppercase'>
+        ✨Hei på deg✨
+      </h1>
+      <div className='flex items-center justify-center space-x-8 p-6'>
+        <Image
+          src={meg.src}
+          width={200}
+          height={200}
+          alt={''}
+          style={{ borderRadius: '100px' }}
+        />
+        <p className='text-lg w-1/2'>
+          Mitt navn er Thea Jenny Kolnes. Jeg tar bachelor i Datateknologi💻 i Bergen på UIB.
+        </p>
       </div>
-
-      <div className='text-secondary'>Made by Thea Jenny Kolnes🦋</div>
-    </main>
+      <div className='flex justify-center space-x-8'>
+        <Button
+          variant='default'
+          size='lg'
+          className='text-base text-text'
+          asChild
+        >
+          <Link
+            href={'https://www.linkedin.com/in/thea-jenny-kolnes-a79821231/'}
+            target='_blank'
+          >
+            <LinkedInLogoIcon className='mr-2 scale-150' />
+            LinkedIn
+          </Link>
+        </Button>
+        <Button
+          variant='default'
+          size='lg'
+          className='text-base text-text'
+          asChild
+        >
+          <Link href={'https://github.com/tjekol'} target='_blank'>
+            <GitHubLogoIcon className='mr-2 scale-150' />
+            Github
+          </Link>
+        </Button>
+        <Button
+          variant='default'
+          size='lg'
+          className='text-base text-text'
+          asChild
+        >
+          <Link href={'mailto:thea.jenny02@gmail.com'}>
+            <EnvelopeClosedIcon className='mr-2 scale-150' />
+            Mail
+          </Link>
+        </Button>
+      </div>
+    </div>
   );
 }
--- my-app/src/components/header.tsx
+++ my-app/src/components/header.tsx
@@ -4,30 +4,30 @@ import { MoonIcon } from '@radix-ui/react-icons';
 
 function Header() {
   return (
-    <div className='flex w-5/6 space-x-2'>
-      <h1 className='grow font-bold text-secondary'>Thea Jenny E. Kolnes</h1>
+    <div className='w-5/6 flex space-x-2'>
+      <h2 className='grow'>Thea Jenny E. Kolnes</h2>
       <Button
         variant='link'
-        className='flex-none text-2xl text-secondary'
+        className='flex-none text-2xl text-text'
         asChild
       >
         <Link href={'/'}>Hjem</Link>
       </Button>
       <Button
         variant='link'
-        className='flex-none text-2xl text-secondary'
+        className='flex-none text-2xl text-text'
         asChild
       >
         <Link href={'/about'}>Om meg</Link>
       </Button>
       <Button
         variant='link'
-        className='flex-none text-2xl text-secondary'
+        className='flex-none text-2xl text-text'
         asChild
       >
-        <Link href={'/project'}>Prosjekter</Link>
+        <Link href={'/projects'}>Prosjekter</Link>
       </Button>
-      <Button size='icon' variant='ghost' className='text-secondary'>
+      <Button size='icon' variant='ghost' className='text-text'>
         <MoonIcon className='scale-150' />
       </Button>
     </div>
