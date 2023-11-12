3e29fa267367ecc54c3f1c714dfd5b5badecae2e
Thea Kolnes <thea.jenny02@gmail.com>
2023-11-12 03:11:26 +0100

    added localStore for darkmode and interactive page nav

--- my-app/src/components/header.tsx
+++ my-app/src/components/header.tsx
@@ -3,40 +3,99 @@
 import Link from 'next/link';
 import { Button } from './ui/button';
 import { MoonIcon } from '@radix-ui/react-icons';
-import { useState } from 'react';
+import { useEffect } from 'react';
 
 function Header() {
-  const [page, setPage] = useState('/');
-  console.log(page);
+  const userTheme = window.localStorage.getItem('isDarkmode');
+  const activePage = window.localStorage.getItem('activePage');
+
+  function isDarkmodeCheck() {
+    if (userTheme !== null) {
+      if (userTheme === 'true') {
+        document.documentElement.classList.add('dark');
+      }
+    }
+  }
+
+  function themeSwitch() {
+    if (document.documentElement.classList.contains('dark')) {
+      document.documentElement.classList.remove('dark');
+      if (userTheme !== null) {
+        window.localStorage.setItem('isDarkmode', 'false');
+      }
+    } else {
+      document.documentElement.classList.add('dark');
+      if (userTheme !== null) {
+        window.localStorage.setItem('isDarkmode', 'true');
+      }
+    }
+  }
+
+  function activePageCheck() {
+    if (activePage !== null) {
+      window.localStorage.setItem('activePage', activePage);
+    }
+  }
+
+  function activePageSet(page: string) {
+    if (activePage !== null) {
+      window.localStorage.setItem('activePage', page);
+    }
+  }
+
+  useEffect(() => {
+    isDarkmodeCheck();
+    activePageCheck();
+  }, []);
 
   return (
-    <div className='w-5/6 flex space-x-2'>
+    <div className='flex w-5/6 space-x-2'>
       <h2 className='grow'>Thea Jenny E. Kolnes</h2>
       <Button
         variant='link'
-        className={'flex-none text-2xl ' + (page === '/' ? 'text-text' : 'text-primary')}
-        onClick={() => setPage('/')}
+        className={
+          'flex-none text-2xl ' +
+          (activePage === '/'
+            ? 'text-text dark:text-text-dark'
+            : 'text-primary dark:text-primary-dark')
+        }
+        onClick={() => activePageSet('/')}
         asChild
       >
         <Link href={'/'}>Hjem</Link>
       </Button>
       <Button
         variant='link'
-        className={'flex-none text-2xl ' + (page === '/about' ? 'text-text' : 'text-primary')}
-        onClick={() => setPage('/about')}
+        className={
+          'flex-none text-2xl ' +
+          (activePage === '/about'
+            ? 'text-text dark:text-text-dark'
+            : 'text-primary dark:text-primary-dark')
+        }
+        onClick={() => activePageSet('/about')}
         asChild
       >
         <Link href={'/about'}>Om meg</Link>
       </Button>
       <Button
         variant='link'
-        className={'flex-none text-2xl ' + (page === '/projects' ? 'text-text' : 'text-primary')}
-        onClick={() => setPage('/projects')}
+        className={
+          'flex-none text-2xl ' +
+          (activePage === '/projects'
+            ? 'text-text dark:text-text-dark'
+            : 'text-primary dark:text-primary-dark')
+        }
+        onClick={() => activePageSet('/projects')}
         asChild
       >
         <Link href={'/projects'}>Prosjekter</Link>
       </Button>
-      <Button size='icon' variant='ghost' className='text-text-text-text'>
+      <Button
+        size='icon'
+        variant='ghost'
+        className='text-text-text-text'
+        onClick={() => themeSwitch()}
+      >
         <MoonIcon className='scale-150' />
       </Button>
     </div>
