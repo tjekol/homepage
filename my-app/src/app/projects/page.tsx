7b48b800e3e0c424419eadfc0d9243b958d86f4c
Thea Kolnes <thea.jenny02@gmail.com>
2023-11-14 00:47:30 +0100

    responsive website

--- my-app/src/app/projects/page.tsx
+++ my-app/src/app/projects/page.tsx
@@ -3,16 +3,16 @@ import Link from 'next/link';
 
 export default function Projects() {
   return (
-    <div className='flex w-full flex-col items-center space-y-8 rounded-[10px] bg-secondary/80 px-10 py-20 dark:bg-secondary-dark'>
+    <div className='flex w-full flex-col items-center space-y-8 rounded-[10px] bg-secondary/80 px-10 py-20 text-center dark:bg-secondary-dark'>
       <h1 className='text-center uppercase'>Prosjekter🔧</h1>
       <div className='flex flex-col items-center space-y-4'>
         <h3 className='text-center'>Webkom💻</h3>
-        <p className='w-1/2 text-center'>
+        <p className='w-4/5 md:w-1/2 '>
           Webkom er en undergruppe under linjeforeningen echo, som er
           linjeforeningen til Institutt for Informatikk på UIB. Denne
           undergruppa drifter nettsidene til echo.
         </p>
-        <div className='flex flex-row space-x-4'>
+        <div className='flex flex-col space-y-4 md:flex-row md:space-x-4 md:space-y-0'>
           <Button>
             <Link href='https://echo.uib.no/'>echo.uib.no</Link>
           </Button>
@@ -23,11 +23,11 @@ export default function Projects() {
       </div>
       <div className='flex flex-col items-center space-y-4'>
         <h3>Memories🎞️</h3>
-        <p className='w-2/3 text-center'>
+        <p className='w-2/3'>
           Dette er en nettside hvor jeg laster opp bilder via Sanity. Nettsiden
           bruker Next.js og tailwindCSS.
         </p>
-        <div className='flex flex-row space-x-4'>
+        <div className='flex flex-col space-y-4 md:flex-row md:space-x-4 md:space-y-0'>
           <Button>
             <Link href='https://memories-tjekol.vercel.app/'>
               memories-tjekol.vercel.app
--- my-app/src/components/ui/button.tsx
+++ my-app/src/components/ui/button.tsx
@@ -16,7 +16,9 @@ const buttonVariants = cva(
           'border border-input bg-background hover:bg-accent hover:text-accent-foreground',
         secondary:
           'bg-secondary text-secondary-foreground hover:bg-secondary/80',
-        ghost: 'hover:bg-accent dark:hover:bg-primary hover:text-primary-foreground dark:hover:text-text',
+        ghost: 'hover:bg-primary/50 dark:hover:bg-primary-dark/50',
+        ghostSecondary:
+          'hover:bg-accent dark:hover:bg-primary hover:text-primary-foreground dark:hover:text-text',
         link: 'text-primary underline-offset-4 hover:underline',
       },
       size: {
