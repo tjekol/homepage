b1d1ed4a58d8e19e230f7094ef12eda03d379ac5
Thea Jenny E Kolnes <thea.jenny02@gmail.com>
2024-06-25 23:08:10 +0200

    mobile-compatible📱

--- my-app/src/app/layout.tsx
+++ my-app/src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body className={inter.className}>
-        <main className='flex min-h-screen flex-col items-center justify-between space-y-8 bg-background p-8 dark:bg-background-dark md:p-10'>
+        <main className='flex min-h-screen flex-col items-center justify-between space-y-8 bg-primary p-8 dark:bg-background-dark md:p-10'>
           <Header />
           <div className='w-full grow md:w-5/6'>{children}</div>
           <div className='text-secondary dark:text-text-dark'>
